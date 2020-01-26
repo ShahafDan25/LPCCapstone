f9e1b90b5e8526f46575b6d511d92b3aef01b48b
Shahaf Dan <dan.shachaf@gmail.com>
2020-01-25 21:53:16 -0800

    push design update

---
+++ captsone.js
@@ -0,0 +1,14 @@
+function helloWorld()
+{
+    console.log("HelloWorld");
+    alert("hello world");
+}
+
+// if(document.getElementById('otherPromotionRadio').checked)
+// {
+//     document.getElementById('otherPromotionText').style.visibility = "visible";
+// }
+// else
+// {
+//     document.getElementById('otherPromotionText').style.visiblity = "hidden";
+// }

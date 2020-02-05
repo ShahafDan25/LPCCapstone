b79d049743c68877073550f574e95f3bea19d280
Shahaf Dan <dan.shachaf@gmail.com>
2020-02-04 18:56:08 -0800

    php sql insertion succesfull. Next step: connect to html

--- capstone.php
+++ capstone.php
@@ -1,41 +1,64 @@
 <?php
     // $servername = "localhost";
+    //$conn; //variable declaration - am I allowed to do this in PHP?
+    //connectDB();
+    insertPat();
+
     function connectDB()
     {
         $username = "root";
         $password = "MMB3189@A";
         $dsn = 'mysql:dbname=TheMarket;host=127.0.0.1;port=3306;socket=/tmp/mysql.sock';
         
-        
+        //try and catch block to connect to PMySQL
         try{
-            $dbh = new PDO($dsn, $username, $password);
+            $conn = new PDO($dsn, $username, $password);
         }
         catch (PDOException $e)
         {
             echo 'Connection Failed: ' . $e -> getMessage();
         }
         //dont forget to close connectio later!
-
+        echo 'connectDB() Completed\n';
         //might need to transfer to html page? should I make that a php file?
     }
 
-    function insertPat($f, $l, $ss, $ca, $aa, $sa, $ea, $pn, $pm)
+    function insertPat(/*$f, $l, $ss, $ca, $aa, $sa, $ea, $pn, $pm*/)
     {
-        $first_name = $f;
-        $last_name = $l;
-        $student_status = $ss;
-        $children_amount = $ca;
-        $adults_amount = $aa;
-        $seniors_amount = $sa;
-        $email_address = $ea;
-        $phone_number = $pn;
-        $promotion_method = $pm;
+        $username = "root";
+        $password = "MMB3189@A";
+        $dsn = 'mysql:dbname=TheMarket;host=127.0.0.1;port=3306;socket=/tmp/mysql.sock';
+        
+        //try and catch block to connect to PMySQL
+        try{
+            $conn = new PDO($dsn, $username, $password);
+        }
+        catch (PDOException $e)
+        {
+            echo 'Connection Failed: ' . $e -> getMessage();
+        }
+        //dont forget to close connectio later!
+        echo 'connectDB() Completed\n';
+        //might need to transfer to html page? should I make that a php file?
+        
+        $first_name = "Frank2"; //filter_input(INPUT_POST, 'first_name');
+        $last_name = "Polanco";
+        $student_status = FALSE;
+        $children_amount = 2;
+        $adults_amount = 2;
+        $seniors_amount = 1;
+        $email_address = NULL;
+        $phone_number = NULL;
+        $promotion_method = NULL;
         //sql insert new patron code
 
-        $sql  = "INSERT INTO Patrons ('FirstName', 'LastName', 'StudentStatus', 'ChildrenAmount', 'AdultsAmount', 'SeniorAmount', EmailAdd', 'PhoneNumber', 'PromotionMethod') VALUE ("
-        . $first_name. $lastname. $student_status. $children_amount. $adults_amount. $seniors_amount. $email_address. $phone_number. $promotion_method. ");";
+        $sql  = "INSERT INTO Patrons (FirstName, LastName, StudentStatus, ChildrenAmount, AdultsAmount, SeniorsAmount, PromotionMethod) VALUES ('".$first_name."', 'Polanco', TRUE, 2, 2, 1, 'Classroom')";
         // just to note: we use the period sign (.) to concatenate in php!!!
-
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->exec($sql); //execute the sql update query
+        
+        
+        echo 'insertPat() Completed\n';
         
     }
     

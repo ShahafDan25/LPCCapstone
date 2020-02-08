6da05d4214148bd38ba65c045a890b9fea115241
Shahaf Dan <dan.shachaf@gmail.com>
2020-02-07 16:42:16 -0800

    connected front and back ends!

--- capstone.php
+++ capstone.php
@@ -6,6 +6,7 @@
     $password = "MMB3189@A";
     $dsn = 'mysql:dbname=TheMarket;host=127.0.0.1;port=3306;socket=/tmp/mysql.sock';
       
+
     //try and catch block to connect to MySQL, or throw an error
     try {
         $conn = new PDO($dsn, $username, $password);
@@ -13,33 +14,34 @@
         echo 'Connection Failed: ' . $e -> getMessage();
     } // end of try and catch
 
-
+    echo "BEGINNING";
+    var_dump($_POST);
     //DATA BASE ACTIOTS POST METHODS
-    if($_POST["message"] == 'insertNewPats')
+    if($_POST['message'] == 'insertNewPats')
     {
         echo "HELLO WORLD OF SUCCESS!";
-        insertPat();
+        insertPat($conn);
     }
 
     /* ***************************************************************** */
     // -------------------------- FUNCTION ACTION -----------------------//
     /* ***************************************************************** */
-    function insertPat()
+    function insertPat($conn)
     {
         
         //dont forget to close connectio later!
-        echo 'connectDB() Completed \n';
+        echo 'insertPat Check';
         //might need to transfer to html page? should I make that a php file?
-        
-        $first_name = filter_input(INPUT_POST, 'first_name');
-        $last_name = filter_input(INPUT_POST, 'last_name'); //retrieve information from HTML
+        var_dump($conn);
+        $first_name = "Hope";//filter_input(INPUT_POST, 'first_name');
+        $last_name = "Fully";//filter_input(INPUT_POST, 'last_name'); //retrieve information from HTML
         $student_status = True;
-        $children_amount = 3;
-        $adults_amount = 5;
-        $seniors_amount = 0;
-        $email_address = "some@email.com";
+        $children_amount = 1;
+        $adults_amount = 2;
+        $seniors_amount = 3;
+        $email_address = "An@email.com";
         $phone_number = NULL;
-        $promotion_method = 'Classroom';
+        $promotion_method = "Testing";
         //sql insert new patron code
 
         $sql  = "INSERT INTO Patrons (FirstName, LastName, StudentStatus, ChildrenAmount, AdultsAmount, SeniorsAmount, PromotionMethod) VALUES 

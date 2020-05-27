22305cead4078a3bcae5b669952e9e760f36e1f6
Shahaf Dan <dan.shachaf@gmail.com>
2020-05-26 20:21:04 -0700

    changes to files

--- connDB.php
+++ connDB.php
@@ -5,11 +5,10 @@ function connDB()
 {
     $username = "root";
     $password = "MMB3189@A";
-    $dsn = 'mysql:dbname=TheMarket;host=127.0.0.1;port=3306;socket=/tmp/mysql.sock';  //connection link
+    // $dsn = 'mysql:dbname=TheMarket;host=127.0.0.1;port=3306;socket=/tmp/mysql.sock';  //connection link
+    $dsn = 'mysql:dbname=TheMarket;host=127.0.0.1;port=3306socket=/tmp/mysql.sock';
     try {$conn = new PDO($dsn, $username, $password);}
     catch (PDOException $e) {echo 'Connection Failed: ' . $e -> getMessage();}
-     
-    
     return $conn;
 }
 

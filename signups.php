95d7bf2ade3573ff4b0883f74c00113214a4901f
Shahaf Dan <dan.shachaf@gmail.com>
2020-06-12 03:05:29 -0700

    continuing signup page; strated profile page

--- signups.php
+++ signups.php
@@ -1,4 +1,4 @@
-<!-- <?php include "funcs.php";?> -->
+<?php include "funcs.php";?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -27,21 +27,28 @@
     </head>
     <body class = "signups-page-body">
         <header class = "nav-bar" style = "margin-right: 5% !important;">
-            <h2 class = "index-registration-page-header-title inline"> Las Positas College: The Market </h2>
-            <h5 class = "index-registration-page-header-sub-title inline"><u> <!-- <?php echo volunteer_name_from_email($_SESSION['volunteer-id']);?> --> </u></h5>
-            <a href = "volunteer-profile.php" class = "nav-bar-option responsive inline" style = "float: right !important;"><i class="fa fa-user" aria-hidden="true"></i> Profile </a>
+            <h2 class = "index-signups-page-header-title"> Las Positas College: The Market </h2>
+            <h5 class = "index-signups-page-header-sub-title"><u> <?php echo volunteer_name_from_email($_SESSION['volunteer-id']);?> </u></h5>
+            <a href = "profile.php" class = "nav-bar-option responsive inline" style = "float: right !important;"><i class="fa fa-user" aria-hidden="true"></i> Profile </a>
             <a href = "index.php" class = "nav-bar-option responsive inline" style = "float: right !important;"><i class="fa fa-home" aria-hidden="true"></i> Main Page </a>
         </header>
         <div class = "page-container">
             <h2> Sign Up To Markets </h2>
             <select class = 'select-markets' name = 'marketid' id = "marketid">
                 <option value = 'none' selected disabled hidden>Choose a Market </option>
-                <!-- <?php echo populateNonTerminatedMarketsDropDown(); ?> -->
+                 <?php echo populateNonTerminatedMarketsDropDown(); ?> 
             </select>
-            <br>
-            <div class = "page-sub-container" id = "signup-sheet-container">
-
+            <br><br>
+            <div class = "page-sub-container" id = "signup-sheet-container-registration" style = "display: none !important;">
+                <form action = "funcs.php" method = "POST">
+                        <input type = "text" class = "index-registration-input half inline" name = "starttime" id = "starttime-input" required>
+                        <input type = "text" class = "index-registration-input half inline" name = "endtime" id = "endtime-input" required>
+                        <input type = "hidden" name = "message" value = "commit-signup">
+                        <button class = "btn submit-admin-login"> Submit </button>
+                </form>
             </div>
+            <br>
+            <div class = "page-sub-container" id = "signup-sheet-container" style = "display: none !important"></div>
         </div>
         <!-- ----------------- FOOTER SECTION --------------------- -->
         <footer class = "footer">
@@ -55,10 +62,15 @@
             $.ajax ({
                 type: "POST",
                 url: "funcs.php",
-                data: {date: document.getElementById("marketid").value, message: "display-signup-sheet"},
+                data: {
+                    message: "display-signup-sheet",
+                    date: document.getElementById("marketid").value, 
+                },
                 success: function(data) {
                     $("#signup-sheet-container").html(data);
-                    // document.getElementById("report-container").hidden = false;
+                    document.getElementById("signup-sheet-container").style.display = block;
+                    document.getElementById("signup-sheet-container-registration").style.display = block;
+
                 }
             });
         });
